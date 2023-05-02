e6b25af33725495fb2e4eb6a6d2fe3b0fe86a801
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-02 17:10:56 +0530

    Time: 17 ms (23.25%) | Memory: 6.4 MB (98.53%) - LeetSync

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -1,68 +1,35 @@
 class Solution {
-    string utilMul(string n1, char c){
-        reverse(n1.begin(), n1.end());
-        int x = c-'0';
-        int n = n1.length();
-        int carry = 0;
-        for(int i=0; i<n; i++){
-            int d = ((n1[i]-'0')*x)+carry;
-            n1[i] = (d%10+'0');
-            carry = d/10;
-        }
-        while(carry != 0){
-            n1 += (carry%10+'0');
-            carry /= 10;
-        }
-        reverse(n1.begin(), n1.end());
-        return n1;
-    }
-    string utilAdd(string n1, string n2){
-        reverse(n1.begin(), n1.end());
-        reverse(n2.begin(), n2.end());
-        int i1=0;
-        int i2=0;
-        int carry = 0;
-        string ans = "";        
-        while(i1<n1.size() && i2<n2.size()){
-            int dig = (n1[i1]-'0')+(n2[i2]-'0')+carry;
-            ans+=to_string(dig%10); 
-            carry = dig/10;
-            i1++;
-            i2++;
-        }
-        while(i1<n1.size()){
-            int dig = (n1[i1]-'0')+carry;
-            ans+=to_string(dig%10); 
-            carry = dig/10;
-            i1++;
-        }
-        while(i2<n2.size()){
-            int dig = (n2[i2]-'0')+carry;
-            ans+=to_string(dig%10); 
-            carry = dig/10;
-            i2++;
-        }
-        while(carry){
-            ans+=to_string(carry%10);
-            carry/=10;
-        }
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
 public:
     string multiply(string num1, string num2) {
         if(num1=="0" || num2 == "0"){
             return "0";
         }
-        if(num2.length() == 1){
-            return utilMul(num1, num2[0]);
+        string ans = "";
+        reverse(num1.begin(),num1.end());
+        reverse(num2.begin(),num2.end());
+        int carry = 0;
+        int i,j;
+        for(i=0; i<num1.size(); i++){
+            carry=0;
+            for(j=0; j<num2.size(); j++){
+                int dig = ((num1[i]-'0')*(num2[j]-'0'))+carry;
+                if(i+j < ans.size()){
+                    dig += (ans[i+j]-'0');
+                    ans[i+j] = ((dig%10)+'0');
+                    carry = dig/10;
+                }
+                else{
+                    ans.push_back((dig%10)+'0');
+                    carry = dig/10;
+                }
+            }
+            while(carry){
+                ans.push_back((carry%10)+'0');
+                carry /= 10;
+            }
+            cout<<ans<<endl;
         }
-        // cout<<utilMul("127",'2');
-        int n = num2.length();
-        string ans1 = utilMul(num1, num2[n-1]);
-        string ans2 = multiply(num1, num2.substr(0, n-1));
-
-        return utilAdd(ans1 , ans2+"0");
-        // return " ";
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
