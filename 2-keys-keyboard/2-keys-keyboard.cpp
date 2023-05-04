1bb792fddcedaaac966acca084bfa4848847ffbe
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-04 17:58:18 +0530

    Time: 19 ms (55.19%) | Memory: 6.5 MB (52.24%) - LeetSync

--- 2-keys-keyboard/2-keys-keyboard.cpp
+++ 2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,21 +1,34 @@
 class Solution {
-    int solve(int n, vector<int>& dp){
-        if(n==1) return 1;
-        if(dp[n] != -1) return dp[n];
+    // int solve(int n, vector<int>& dp){
+    //     if(n==1) return 1;
+    //     if(dp[n] != -1) return dp[n];
         
-        int ans = n;
-        for(int i=n/2; i>=2; i--){
-            if(n%i == 0){
-                int ops = (n/i)-1 + 1;
-                ans = min(ans, ops + solve(i,dp));
-            }
-        }
-        return dp[n] = ans;
-    }
+    //     int ans = n;
+    //     for(int i=n/2; i>=2; i--){
+    //         if(n%i == 0){
+    //             int ops = (n/i)-1 + 1;
+    //             ans = min(ans, ops + solve(i,dp));
+    //         }
+    //     }
+    //     return dp[n] = ans;
+    // }
 public:
     int minSteps(int n) {
         if(n==1) return 0;
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n+1,1);
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            int ans = i;
+            for(int j=i/2; j>=2; j--){
+                if(i%j == 0){
+                    int ops = (i/j)-1 + 1;
+                    ans = min(ans, ops + dp[j]);
+                }
+            }
+            dp[i] = ans;
+        }
+        
+        return dp[n];
     }
 };
