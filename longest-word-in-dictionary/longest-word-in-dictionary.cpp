1014248c76efc24248b9b21d89ebd8259ce00737
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-05 19:04:46 +0530

    Time: 49 ms (70.10%) | Memory: 31.1 MB (36.49%) - LeetSync

---
+++ longest-word-in-dictionary/longest-word-in-dictionary.cpp
@@ -0,0 +1,50 @@
+class TrieNode{
+    public:
+    TrieNode* next[26];
+    bool isTerminal;
+    TrieNode(){
+        this->isTerminal = false;
+        for(int i=0; i<26; i++) this->next[i] = NULL;
+    }
+};
+class Trie{
+    public: 
+    TrieNode* root;
+    Trie(vector<string>& words){
+        this->root = new TrieNode();
+        for(auto word: words){
+            this->addWord(word);
+        }
+    }
+    void addWord(string s){
+        TrieNode* rt = root;
+        for(int i=0; i<s.size(); i++){
+            if(rt->next[s[i]-'a'] == NULL){
+                rt->next[s[i]-'a'] = new TrieNode();
+            }
+            rt = rt->next[s[i]-'a'];
+        }
+        rt->isTerminal = true;
+    }
+    string solve(TrieNode* rt){
+        string ans = "";
+        for(int i=0; i<26; i++){
+            if((rt->next[i] != NULL) && (rt->next[i]->isTerminal == true)){
+                string temp = solve(rt->next[i]);
+                temp.push_back(char('a'+i));
+                if(ans.size() < temp.size()) 
+                    ans = temp;
+            }
+        }
+        return ans;
+    }
+};
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        Trie* tr = new Trie(words);
+        string ans = tr->solve(tr->root);
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
