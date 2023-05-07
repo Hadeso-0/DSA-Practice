2c7682029cf7c2f67c7f997932d86ec3c0bc42d5
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-07 17:30:55 +0530

    Time: 68 ms (45.47%) | Memory: 25 MB (25.84%) - LeetSync

---
+++ longest-word-in-dictionary-through-deleting/longest-word-in-dictionary-through-deleting.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    bool lcs(string s, string target){
+        int n = s.size();
+        int m = target.size();
+        int i,j;
+        for(i=0, j=0; i<n && j<m; ){
+            if(s[i] == target[j]){
+                j++;
+            }
+            i++;
+        }
+        return j == m;
+    }
+public:
+    string findLongestWord(string s, vector<string>& dictionary) {
+
+        string ans = "";
+        for(auto word : dictionary){
+            if(((ans.size() < word.size()) || (ans.size() == word.size() && ans.compare(word) > 0)) && lcs(s, word)){
+                ans = word;
+            }
+        }
+        return ans;
+    }
+};
