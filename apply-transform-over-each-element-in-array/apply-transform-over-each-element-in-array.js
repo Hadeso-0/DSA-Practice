eea207c10b032c2683100611828d6fb3785f7bf4
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-09 18:59:35 +0530

    Time: 61 ms (39.30%) | Memory: 41.9 MB (70.07%) - LeetSync

---
+++ apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let ans = [];
+
+    arr.forEach((n,i)=>{
+        ans.push(fn(n,i));
+    })
+    return ans;
+};
