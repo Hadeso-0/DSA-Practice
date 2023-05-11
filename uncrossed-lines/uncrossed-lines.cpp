d6dd5a690e2b81427d673e657646d8d3ed27a8f3
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-11 10:56:20 +0530

    Time: 16 ms (78.63%) | Memory: 13 MB (63.96%) - LeetSync

--- uncrossed-lines/uncrossed-lines.cpp
+++ uncrossed-lines/uncrossed-lines.cpp
@@ -1,39 +1,20 @@
 class Solution {
-    int findIdx(int target, vector<int>& nums, int s){
-        for(int i=s; i<nums.size(); i++){
-            if(nums[i] == target) return i;
-        }
-        return -1;
-    }
-    
-    int solve(int idx, vector<int>& nums1, vector<int>& nums2, int idx2, vector<vector<int>>& dp){
-        if(idx >= nums1.size() || idx2 >= nums2.size()){
-            return 0;
-        }
-        if(dp[idx][idx2] != -1) return dp[idx][idx2];
-
-        // If i include this number in a connection
-        int incl = 0;
-        int idx2_new = findIdx(nums1[idx], nums2, idx2); 
-        if(idx2_new != -1){ // its pair exist in nums2 after idx2
-            incl = dp[idx+1][idx2_new+1];
-            if(incl == -1)
-                incl = solve(idx+1, nums1, nums2, idx2_new+1, dp); // idx2 is updated one
-            
-            ++incl;
-        }
-
-        // If I exclude this number in a connection or its pair does not exist
-        int excl = dp[idx+1][idx2];
-        if(excl == -1)
-            excl = solve(idx+1, nums1, nums2, idx2, dp);
-
-        return dp[idx][idx2] = max(incl, excl);
-    }
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size()) swap(nums1,nums2);
-        vector<vector<int>> dp(nums1.size()+1, vector<int>(nums2.size()+1, -1));
-        return solve(0,nums1, nums2, 0, dp);
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
