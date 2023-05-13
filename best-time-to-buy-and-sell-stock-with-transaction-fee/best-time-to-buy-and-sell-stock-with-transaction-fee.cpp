98ed853a9c35db4e5dbfc9dc1a3881a7035f7821
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-13 15:16:30 +0530

    Time: 121 ms (74.41%) | Memory: 54.9 MB (92.97%) - LeetSync

--- best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -6,20 +6,14 @@ public:
         // return solve(0,1,fee,prices, dp);
         
         int gain, skip;        
-        vector<vector<int>> dp(2, vector<int>(2, 0));
+        int buy = 0;
+        int sell = 0;
+        
         for(int i = n-1; i>=0; i--){
-            for(int signal = 0; signal <= 1; signal++){
-                gain = 0;
-                if(signal){
-                    gain = dp[(i+1)%2][1-signal] - prices[i];
-                }
-                else{
-                    gain = dp[(i+1)%2][1-signal]+ prices[i] - fee;
-                }
-                skip = dp[(i+1)%2][signal];
-                dp[i%2][signal] = max(gain, skip);
-            }
+            int temp = sell;
+            sell = max(buy + prices[i] - fee , sell);
+            buy  = max(temp - prices[i] , buy);
         }
-        return dp[0][1];
+        return buy;
     }
 };
