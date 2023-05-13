81de587c47d31fc9d595c01c1e4ef9bf665920ad
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-13 22:30:28 +0530

    Time: 14 ms (78.51%) | Memory: 6.4 MB (98.85%) - LeetSync

--- count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
+++ count-ways-to-build-good-strings/count-ways-to-build-good-strings.cpp
@@ -3,15 +3,16 @@ class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
         
-        vector<int> dp(high+1, 0);
+        int dp[100001] = {0};
         dp[0] = 1;
-        for(int i= 0; i<=high; i++){
+        for(int i= 1; i<=high; ++i){
             if(i >= zero) dp[i] = (dp[i] + dp[i-zero])%mod;
             if(i >= one)  dp[i] = (dp[i] + dp[i-one])%mod;
         }
-        for(int i = low+1; i<=high; i++) 
-            dp[i] = (dp[i] + dp[i-1])%mod;
+        int ans = 0;
+        for(int i = low; i<=high; ++i) 
+            ans = (ans + dp[i])%mod;
         
-        return dp[high];
+        return ans;
     }
 };
