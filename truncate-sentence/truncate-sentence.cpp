bf937edccdaee23f86d74662a3ee0fda8fce3708
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-14 11:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (67.07%) - LeetSync

--- truncate-sentence/truncate-sentence.cpp
+++ truncate-sentence/truncate-sentence.cpp
@@ -4,8 +4,10 @@ public:
         string ans = "";
         int sps = 0;
         for(int i=0; i<s.size(); i++){
-            if(s[i] == ' ') sps++;
-            if(sps == k) break;
+            if(s[i] == ' '){ 
+                sps++;
+                if(sps == k) break;
+            }
             ans += s[i];
         }
         return ans;
