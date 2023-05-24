e78b68ab8b8e94133bdcfe2f21858b68070275f3
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-24 18:46:54 +0530

    Time: 24 ms (49.80%) | Memory: 14.5 MB (21.25%) - LeetSync

--- snakes-and-ladders/snakes-and-ladders.cpp
+++ snakes-and-ladders/snakes-and-ladders.cpp
@@ -7,43 +7,41 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n =  board.size();
-        // vector<vector<int>> brd((n*n)+1);
-        // for(int i=1; i<=(n*n); ++i){
-        //     for(int j=1; j<=6; ++j){
-        //         if(i+j > (n*n)) break;
-        //         brd[i].push_back(i+j);
-        //     }
-        // }
+
 
         unordered_map<int,int> mp;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(board[i][j] != -1){
                     int src = genVal(i,j,n);
-                    // cout<<src<<"->"<<board[i][j]<<endl;
                     mp[src] = board[i][j];
                 }
             }
         }  
 
-        queue<pair<int,int>> q;
-        q.push({1, 0});
+        queue<int> q;
+        q.push(1);
         vector<bool> vis((n*n)+1, false);
         vis[1] = true;
-        while(!q.empty()){
-            int cur = q.front().first;
-            int moves = q.front().second;
-            // cout<<cur<<"("<<moves<<")"<<"-> ";
-            q.pop();
-            for(int i=1; i<=6; ++i){
-                int nex = cur + i;
+        int ans = 0;
 
-                if(mp[nex] != 0) nex = mp[nex];
-                if(vis[nex]) continue;
-                // cout<<nex<<", ";
-                if(nex == (n*n)) return (moves+1);
-                q.push({nex, moves+1});
-                vis[nex] = true;
+        while(!q.empty()){
+            ++ans;
+            for(int i=q.size()-1; i>=0; --i){
+                int cur = q.front();
+                // cout<<cur<<" ";
+                q.pop();
+
+                for(int i=1; i<=6; ++i){
+                    int nex = cur + i;
+
+                    if(mp[nex] != 0) nex = mp[nex];
+
+                    if(nex == (n*n)) return ans; 
+                    if(vis[nex]) continue;
+                    q.push(nex);
+                    vis[nex] = true;
+                }
             }
             // cout<<endl;
         }
