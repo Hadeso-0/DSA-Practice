439fefd08d782678db15c5bddd2aca1f83347f42
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-25 20:31:56 +0530

    Time: 7 ms (97.85%) | Memory: 22.1 MB (98.80%) - LeetSync

--- house-robber-iii/house-robber-iii.cpp
+++ house-robber-iii/house-robber-iii.cpp
@@ -10,37 +10,33 @@
  * };
  */
 class Solution {
-    int solve(TreeNode* root, bool canInclude, unordered_map<TreeNode*, pair<int,int>>& dp){
-        if(root == NULL) return 0;
+    pair<int,int> solve(TreeNode* root){
+        if(root == NULL) return {0, 0};
 
-        if(canInclude) {
-            if(dp[root].first != -1) return dp[root].first;
-        }
-        else{
-            if(dp[root].second != -1) return dp[root].second;
-        }
+        pair<int,int> lft = solve(root->left);  
+        pair<int,int> rgt = solve(root->right);
+        
+        pair<int,int> ans;
+        
+        // include this val
+        ans.first = root->val + lft.second + rgt.second;
+        
+        // exclude this val
+        int v1 = lft.first + rgt.first;
+        int v2 = lft.first + rgt.second;
+        int v3 = lft.second + rgt.first;
+        int v4 = lft.second + rgt.second;
+        ans.second = max(v1, max(v2, max(v3, v4)));
 
-        int incl = 0;
-        int excl = solve(root->left, 1, dp) + solve(root->right, 1, dp);
-        // if can include -> I can include or exclude
-        if(canInclude){
-            incl = root->val + solve(root->left, 0, dp) + solve(root->right, 0, dp);
-            return dp[root].first = max(incl, excl);
-        }
-
-        // if cannot include -> I can only exclude
-        return dp[root].second = excl;
-    }
-    void init_map(TreeNode* root, unordered_map<TreeNode*, pair<int,int>>& dp){
-        if(root == NULL) return;
-        dp[root] = {-1,-1};
-        init_map(root->left, dp);
-        init_map(root->right, dp);
+        return ans;
     }
 public:
     int rob(TreeNode* root) {
-        unordered_map<TreeNode*, pair<int, int>> dp;
-        init_map(root, dp);
-        return solve(root, 1, dp);        
+        // unordered_map<TreeNode*, pair<int, int>> dp;
+        // pair<int,int> init_val = {-1,-1};
+        // init_map(root, dp, init_val);
+        
+        pair<int,int> ans = solve(root);
+        return max(ans.first, ans.second);        
     }
 };
