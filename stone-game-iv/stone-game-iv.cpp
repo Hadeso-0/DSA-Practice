3987b4ba22a1787780b93f1a51add7ce4f425d21
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-27 01:33:38 +0530

    Time: 63 ms (62.31%) | Memory: 6.3 MB (89.72%) - LeetSync

--- stone-game-iv/stone-game-iv.cpp
+++ stone-game-iv/stone-game-iv.cpp
@@ -8,10 +8,18 @@ public:
             bool temp = true;
             for(int j=1; (j*j) <= i; ++j){
                 temp &= dp[i-(j*j)];
+                if(!temp) break;
             }
             dp[i] = (1-temp);
         }
         // for(int i=0; i<=n; ++i) cout<<dp[i]<<" ";
         return dp[n];
+
+        // for(int i=0; i<=n; ++i){
+        //     if(dp[i]) continue;
+        //     for(int k=1; i+(k*k)<=n; ++k)
+        //         dp[i+(k*k)] = true;
+        // }
+        // return dp[n];
     }
 };
