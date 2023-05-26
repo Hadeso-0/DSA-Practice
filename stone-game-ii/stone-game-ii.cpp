68961d75bade2b7567a80f8fa61c0660157bb024
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-26 21:40:21 +0530

    Time: 15 ms (82.71%) | Memory: 11.7 MB (34.57%) - LeetSync

---
+++ stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int solve(vector<int>& piles, vector<int>& suffSum, int idx, int M, vector<vector<int>>& dp){
+        if(idx >= piles.size()) return 0;
+
+        if(dp[idx][M] != -1) return dp[idx][M];
+
+        int ans = INT_MIN;
+        int cur_sum = 0;
+        for(int x=1; x<=(2*M); ++x){
+            if(idx + x > piles.size()) break;
+            cur_sum += piles[idx+x-1];
+            int M_new = max(x, M);
+            int temp = solve(piles, suffSum, idx+x, M_new, dp);
+            ans = max(ans, cur_sum+suffSum[idx+x]-temp);
+        }
+        return dp[idx][M] = ans;
+    }
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<int> sufSum(n+1, 0);
+        for(int i=n-1; i>=0; --i){
+            sufSum[i] = sufSum[i+1] + piles[i];
+        }
+        vector<vector<int>> dp(n+1, vector<int>((2*n)+1, -1));
+        return solve(piles, sufSum, 0, 1, dp);
+    }   
+};
