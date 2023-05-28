7243cd47cd103db60c287784d275408519ee4b0d
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-29 04:11:25 +0530

    Time: 202 ms (18.08%) | Memory: 42.2 MB (16.86%) - LeetSync

---
+++ nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& st) {
+        if(maze[st[0]][st[1]] == '+') return -1;
+        
+        int n = maze.size();
+        int m = maze[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
+        int ans = 0;
+
+        queue<vector<int>> q;
+        q.push(st);
+        vis[st[0]][st[1]] = true;
+        
+        vector<int> dx = {1,0,-1,0};
+        vector<int> dy = {0,1,0,-1};
+        
+        while(!q.empty()){
+            ++ans;
+            for(int i=q.size()-1; i>=0; --i){
+                vector<int> tp = q.front();
+                q.pop();
+                for(int d = 0; d<4; ++d){
+                    int x = tp[0] + dx[d];
+                    int y = tp[1] + dy[d];
+                
+                    // check if valid
+                    if((x>=0) && (y>=0 )&& (x<n) && (y<m) && (!vis[x][y]) && (maze[x][y] != '+')){
+                        if(x==0 || y==0 || x==n-1 || y==m-1){// if is an exit
+                            return ans;
+                        }
+                        q.push({x,y});
+                        vis[x][y] = true;
+                    }
+                }
+            }
+        }
+  
+        return -1;
+    }
+};
