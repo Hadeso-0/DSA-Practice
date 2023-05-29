25d75988cbbfb3837a2a67d85d55441901d81915
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-05-29 13:13:23 +0530

    Time: 62 ms (62.00%) | Memory: 33 MB (60.01%) - LeetSync

--- design-parking-system/design-parking-system.cpp
+++ design-parking-system/design-parking-system.cpp
@@ -2,17 +2,11 @@ class ParkingSystem {
     vector<int> slots;
 public:
     ParkingSystem(int big, int medium, int small) {
-        slots.push_back(big);
-        slots.push_back(medium);
-        slots.push_back(small);
+        slots = {big,medium,small};
     }
     
     bool addCar(int carType) {
-        if(slots[carType-1]  > 0){
-            --slots[carType-1];
-            return true;
-        }
-        return false;
+        return slots[carType-1]  > 0 ? slots[carType-1]-- : false;
     }
 };
 
