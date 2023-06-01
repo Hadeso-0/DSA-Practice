eccd93aafb8e525beea2baeb301506a200c689e0
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-01 13:56:45 +0530

    Time: 432 ms (5.28%) | Memory: 21.1 MB (56.45%) - LeetSync

---
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    vector<int> dx = {1,1,0,-1,-1,-1,0,1};
+    vector<int> dy = {0,1,1,1,0,-1,-1,-1};
+    bool isValid(int i, int j, vector<vector<int>>& grid){
+        if(i>=0 && j>=0 && i<grid.size() && j<grid.size() && grid[i][j]!=1)
+            return true;
+        return false;
+    }
+    void bfs(vector<vector<int>>& grid, vector<vector<int>>& dp, vector<vector<bool>>& vis){
+
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        dp[0][0] = 1;
+        vis[0][0] = 1;
+        
+        while(!q.empty()){
+
+            pair<int,int> cell = q.front();
+            q.pop();
+
+            int x = cell.first;
+            int y = cell.second;
+            cout<<x<<" "<<y<<endl;
+            for(int i=0; i<8; i++){
+                if(isValid(x+dx[i], y+dy[i], grid) && !vis[x+dx[i]][y+dy[i]]){
+                    q.push({x+dx[i], y+dy[i]});
+                    vis[x+dx[i]][y+dy[i]] = true;
+                    dp[x+dx[i]][y+dy[i]] = min(dp[x+dx[i]][y+dy[i]], dp[x][y]+1);
+                }
+            }
+        }
+    }
+
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1) return -1;
+        if(n == 1) return 1;
+        vector<vector<int>> dp(n, vector<int>(n,INT_MAX));
+        vector<vector<bool>> vis(n, vector<bool>(n,false));
+        bfs(grid, dp, vis);
+        return dp[n-1][n-1]==INT_MAX?-1:dp[n-1][n-1];
+    }
+};
