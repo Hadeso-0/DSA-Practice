a54dc694fb9345b0f4a2e77559240a7ed629ea50
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-01 17:39:57 +0530

    Time: 75 ms (6.10%) | Memory: 41.9 MB (52.34%) - LeetSync

---
+++ debounce/debounce.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+const debounce = (fn, t) => {
+    let timer = null
+    return (...args) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn(...args)
+        }, t)
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
