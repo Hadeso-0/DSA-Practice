b07103aab05c393b3721b0be5384ac6d4a1109f6
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-04 15:53:59 +0530

    Time: 733 ms (9.09%) | Memory: 212.5 MB (9.09%) - LeetSync

--- sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
+++ sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& queries) {
         long long ans = 0;
-        unordered_map<int, int> rows;
-        unordered_map<int, int> cols;
+        vector<unordered_set<int>> mark(2);
+        
         for(int q = queries.size()-1; q>=0; --q){
-            if(rows.size() == n && cols.size() == n) break;
-            if(queries[q][0] == 0){
-                if(rows.find(queries[q][1]) == rows.end()){
-                    ans += (n-cols.size())*queries[q][2];
-                    rows[queries[q][1]] = queries[q][2];
-                }
-            }
-            else{
-                if(cols.find(queries[q][1]) == cols.end()){ 
-                    ans += (n-rows.size())*queries[q][2];
-                    cols[queries[q][1]] = queries[q][2];
-                }
+            if(mark[0].size() == n && mark[1].size() == n) break;
+            if(mark[queries[q][0]].find(queries[q][1]) == mark[queries[q][0]].end()){
+                ans += (n-mark[1 - queries[q][0]].size())*queries[q][2];
+                mark[queries[q][0]].insert(queries[q][1]);
             }
         }
         return ans;
