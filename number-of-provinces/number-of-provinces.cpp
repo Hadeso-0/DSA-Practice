f37965677cbfc549af1c730318c66423b91e5145
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-04 15:00:22 +0530

    Time: 28 ms (51.98%) | Memory: 13.7 MB (71.32%) - LeetSync

--- number-of-provinces/number-of-provinces.cpp
+++ number-of-provinces/number-of-provinces.cpp
@@ -4,8 +4,9 @@ class Solution {
         vis[i] = true;
         while(!q.empty()){
             int fr = q.front(); q.pop();
-            for(auto& j : g[fr]){
-                if(!vis[j]){
+            cout<<fr<<" ";
+            for(int j=0; j < g[fr].size(); ++j){
+                if(fr!=j && !vis[j] && g[fr][j] == 1){
                     q.push(j);
                     vis[j] = true;
                 }
@@ -13,26 +14,27 @@ class Solution {
         }
     }
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
+    int findCircleNum(vector<vector<int>>& g) {
+        int n = g.size();
         vector<bool> vis(n, false);
 
-        vector<vector<int>> g(n);
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<n; ++j){
-                if(i == j) continue;
-                if(isConnected[i][j]){ 
-                    g[i].push_back(j);
-                    g[j].push_back(i);
-                }
-            }
-        }
+        // vector<vector<int>> g(n);
+        // for(int i=0; i<n; ++i){
+        //     for(int j=0; j<n; ++j){
+        //         if(i == j) continue;
+        //         if(isConnected[i][j]){ 
+        //             g[i].push_back(j);
+        //             g[j].push_back(i);
+        //         }
+        //     }
+        // }
         int ans = 0;
         queue<int> q;
         for(int i=0; i<n; ++i){
             if(!vis[i]){
                 ++ans;
                 bfs(i, g, vis, q);
+                cout<<endl;
             }
         }
         return ans;
