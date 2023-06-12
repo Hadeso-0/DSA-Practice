009a7f889c5d7cfc2defd1411b02126fcea81d32
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-12 19:12:32 +0530

    Time: 449 ms (16.89%) | Memory: 72.5 MB (38.62%) - LeetSync

---
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    bool comp(const vector<int>& a, const vector<int>& b){
+        cout<<(a[0]*1LL*a[0] + a[1]*1LL*a[1])<<" "<<(b[0]*1LL*b[0] + b[1]*1LL*b[1]);
+        return (a[0]*1LL*a[0] + a[1]*1LL*a[1]) < (b[0]*1LL*b[0] + b[1]*1LL*b[1]);
+    }
+    long long dis(const vector<int>& a){
+        return (a[0]*1LL*a[0] + a[1]*1LL*a[1]);
+    }
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<long long,vector<int>>> pq;
+        int n = points.size();
+        for(int i=0; i<n; ++i){
+            if(pq.size() < k){
+                pq.push({dis(points[i]), points[i]});
+            }
+            else{
+                if(comp(points[i],(pq.top()).second)){
+                    cout<<i<<" ";
+                    pq.pop();
+                    pq.push({dis(points[i]), points[i]});
+                }
+            }
+        }
+        vector<vector<int>> ans;
+        while(!pq.empty()){ 
+            ans.push_back((pq.top()).second);
+            pq.pop();
+        }
+        return ans;
+
+
+        // sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b){
+        //     return (a[0]*1LL*a[0] + a[1]*1LL*a[1]) < (b[0]*1LL*b[0] + b[1]*1LL*b[1]);
+        // });
+        // int n = points.size();
+        // vector<vector<int>> ans;
+        // for(int i=0; i<min(n,k); ++i) ans.push_back(points[i]);
+        // return ans;
+    }
+};
