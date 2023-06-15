f8801af686cdc862984f9fcf1a3476792f0cdcd1
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-15 12:37:32 +0530

    Time: 186 ms (94.93%) | Memory: 65.6 MB (98.19%) - LeetSync

--- diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        map<int,vector<int>> mp;
-        for(int i=0; i<nums.size(); ++i){
-            for(int j=0; j<nums[i].size(); ++j){
-                mp[i+j].push_back(nums[i][j]);
-            }
-        }   
         vector<int> ans;
-        for(auto& dgs : mp){
-            for(int i = (dgs.second).size()-1; i>=0; --i){
-                ans.push_back((dgs.second)[i]);
-            }
+        queue<pair<int,int>> q;
+        q.push({0,0});
+
+        while(!q.empty()){
+            int r = (q.front()).first;
+            int c = (q.front()).second;
+            q.pop(); ans.push_back(nums[r][c]);
+            if(c == 0 && r+1 < nums.size()) q.push({r+1,0});
+            if(c+1 < nums[r].size()) q.push({r,c+1});
         }
         return ans;
     }
