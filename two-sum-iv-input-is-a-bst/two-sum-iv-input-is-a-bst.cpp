d07115ecb9b0f719e6fae7b19509d1b3a14aa6fa
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-16 11:24:17 +0530

    Time: 57 ms (17.17%) | Memory: 39.1 MB (13.88%) - LeetSync

--- two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -10,24 +10,29 @@
  * };
  */
 class Solution {
-    void solve(TreeNode* root, vector<int>& arr){
-        if(root == NULL) return;
-        solve(root->left, arr);
-        arr.push_back(root->val);
-        solve(root->right, arr);
+    bool solve(TreeNode* root, set<int>& st, int k){
+        if(root == NULL) return false;
+
+        if(solve(root->left, st, k)) return true;
+        
+        if(st.find(root->val) != st.end()) return true;
+        st.insert(k - root->val);
+        
+        return solve(root->right, st, k);
     }
 public:
     bool findTarget(TreeNode* root, int k) {
         // Brute force -> find preorder -> apply 2 pointer
-        vector<int> pre;
-        solve(root, pre);
+        // vector<int> pre;
+        set<int> vals;
+        return solve(root, vals, k);
 
-        int i= 0; int j = pre.size()-1;
-        while(i < j){
-            if(pre[i] + pre[j] == k) return true;
-            if(pre[i] + pre[j] > k) --j;
-            else ++i;
-        }   
-        return false;
+        // int i= 0; int j = pre.size()-1;
+        // while(i < j){
+        //     if(pre[i] + pre[j] == k) return true;
+        //     if(pre[i] + pre[j] > k) --j;
+        //     else ++i;
+        // }   
+        // return false;
     }
 };
