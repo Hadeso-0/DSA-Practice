9faa0c899f6303f2444b319bc83adcf303d03431
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-25 13:52:03 +0530

    Time: 229 ms (16.67%) | Memory: 8.3 MB (16.67%) - LeetSync

--- construct-the-longest-new-string/construct-the-longest-new-string.cpp
+++ construct-the-longest-new-string/construct-the-longest-new-string.cpp
@@ -1,18 +1,18 @@
 class Solution {
+    int dp[51][51][51][4] = {};
+    int solve(int prev, int x, int y, int z){
+        if(dp[x][y][z][prev] != -1) return dp[x][y][z][prev];
+
+        int ans = 0;
+        if(x>0 && prev != 1) ans = max(ans, solve(1,x-1,y,z)+2);
+        if(y>0 && prev <= 1) ans = max(ans, solve(2,x,y-1,z)+2);
+        if(z>0 && prev != 1) ans = max(ans, solve(3,x,y,z-1)+2);
+
+        return dp[x][y][z][prev] = ans;
+    }
 public:
     int longestString(int x, int y, int z) {
-        int ans = 2*z;
-       
-        if(x > y){
-            ans += 2*(y +1 + y);
-        }
-        else if(x==y){
-            ans += 2*(x + y);
-        }
-        else{
-            ans += 2*(x + x + 1);
-        }
-       
-        return ans;
+        memset(dp,-1,sizeof(dp));
+        return solve(0, x, y, z);
     }
 };
