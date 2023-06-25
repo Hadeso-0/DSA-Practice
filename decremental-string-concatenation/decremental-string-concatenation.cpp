fdadd3b2a44ae1f3c1612d88365ebe3f84898cd5
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-25 16:42:10 +0530

    Time: 339 ms (77.78%) | Memory: 40.7 MB (88.89%) - LeetSync

--- decremental-string-concatenation/decremental-string-concatenation.cpp
+++ decremental-string-concatenation/decremental-string-concatenation.cpp
@@ -1,35 +1,61 @@
 class Solution {
     int dp[26][26][1005] = {};
-    int solve(char fc, char lc, int idx, vector<string>& words){
+    /**
+     * @remark 
+     * function = solve() returns integer(maximum number of characters that can be deleted)
+     *            returns the maximum overlapping characters that can be acheived on merging
+     *            strings
+     *
+     * params = first_char -> char at first index of str_idx-1
+     *        = last_char -> char at end index of str_idx-1
+     *        = idx -> index of the word under observation
+     */
+    int solve(char first_char, char last_char, int idx, vector<string>& words){
 
-        // cout<<idx<<" "<<fc<<" "<<lc<<endl;
-        int pos1=0,pos2=0, pos3 = 0;
         if(idx == words.size()) return 0;
-        if(dp[fc-'a'][lc-'a'][idx] != -1) return dp[fc-'a'][lc-'a'][idx];
+       
+        // Memoization
+        if(dp[first_char-'a'][last_char-'a'][idx] != -1) 
+            return dp[first_char-'a'][last_char-'a'][idx];
 
-        pos1 = solve( fc, words[idx][words[idx].size()-1], idx+1, words);
-        if(words[idx][0] == lc){
-            ++pos1; 
+
+        // If I take s_idx = s_idx-1 + words[idx];
+        // and find maximum overlapping chars with currently formed 
+        // string and words after this
+        int way1 = solve( first_char, words[idx][words[idx].size()-1], idx+1, words);
+        if(words[idx][0] == last_char){
+            // Remove the last char of s_idx-1, if overlap
+            ++way1; 
         }
         
-        pos2 = solve( words[idx][0], lc, idx+1, words);
-        if(words[idx][words[idx].size()-1] == fc){
-            ++pos2;
+        //  If I take s_idx = words[idx] + s_idx-1; 
+        // and find maximum overlapping chars with currently formed 
+        // string and words after this
+        int way2 = solve( words[idx][0], last_char, idx+1, words);
+        if(words[idx][words[idx].size()-1] == first_char){
+            // Remove the first char of s_idx-1, if overlap
+            ++way2;
         }     
         
-        return dp[fc-'a'][lc-'a'][idx] = max(pos1, pos2);
+        return dp[first_char-'a'][last_char-'a'][idx] = max(way1, way2);
     }
 public:
     int minimizeConcatenatedLength(vector<string>& words) {
-        char fc = words[0][0]; char lc = words[0][words[0].size()-1];
+        
+        // Take 1st word as str_0
+        char first_char = words[0][0]; 
+        char last_char = words[0][words[0].size()-1];
+
+        // Reset dp
         memset(dp, -1, sizeof(dp));
 
         int n = words.size();
-        int len = 0;
-        for(int i=0; i<words.size(); ++i){
-            len += words[i].size();
+        int total_length = 0; // Maximum possible length
+        for(int i=0; i<n; ++i){
+            total_length += words[i].size();
         }
         
-        return len-solve( fc, lc, 1, words);
+        // Minimum overlapping length = Total length - Maximum overlapping characters
+        return total_length - solve( first_char, last_char, 1, words);
     }
 };
