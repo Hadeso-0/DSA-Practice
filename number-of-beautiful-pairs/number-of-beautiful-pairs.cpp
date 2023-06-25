c9cd894ac5a55ebc8227718883d478057ca9c55f
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-06-25 18:35:20 +0530

    Time: 45 ms (87.50%) | Memory: 69.2 MB (25.00%) - LeetSync

--- number-of-beautiful-pairs/number-of-beautiful-pairs.cpp
+++ number-of-beautiful-pairs/number-of-beautiful-pairs.cpp
@@ -3,15 +3,18 @@ public:
     int countBeautifulPairs(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
+        unordered_map<int,int> mp;
         for(int i=0; i<n; ++i){
-            for(int j=i+1; j<n; ++j){
-                string n1 = to_string(nums[i]);
-                string n2 = to_string(nums[j]); 
-                if(__gcd(n1[0]-'0', n2[n2.size()-1]-'0') == 1){
-                    // cout<<n1[0]<<" "<<i<<" "<<n2[n2.size()-1]<<" "<<j<<endl;
-                    ++ans;
+            int fd = nums[i]%10;
+            while(nums[i]>=10) nums[i]/=10;
+            int ld = nums[i];
+            
+            for(int x=1; x<=9; ++x){
+                if(__gcd(x, fd) == 1){
+                    ans += mp[x];
                 }
             }
+            ++mp[ld];
         }   
         return ans;
     }
