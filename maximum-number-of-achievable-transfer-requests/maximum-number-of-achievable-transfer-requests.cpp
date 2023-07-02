0da9cd531a5edb28059887b1922d83ac34fe6081
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-03 01:25:45 +0530

    Time: 597 ms (38.89%) | Memory: 182.9 MB (29.63%) - LeetSync

---
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        int answer = 0; 
+        
+        for (int mask = 0; mask < (1 << requests.size()); mask++) {
+            vector<int> indegree(n, 0);
+            int pos = requests.size() - 1;
+            int bitCount = __builtin_popcount(mask);
+            
+            if (bitCount <= answer) {
+                continue;
+            }
+            for (int curr = mask; curr > 0; curr >>= 1, pos--) {
+                if (curr & 1) {
+                    indegree[requests[pos][0]]--;
+                    indegree[requests[pos][1]]++;
+                }
+            }
+            
+            int flag = 1;
+            for (int i = 0; i < n; i++) {
+                if (indegree[i]) {
+                    flag = 0;
+                    break;
+                }
+            }
+            
+            if (flag)  {
+                answer = bitCount;
+            }
+        }
+        
+        return answer;
+    }
+};
