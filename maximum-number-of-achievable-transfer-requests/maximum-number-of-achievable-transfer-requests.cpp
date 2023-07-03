b6b323ca89366c261fd5490a7e1b62eb6bda5402
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-03 12:00:10 +0530

    Time: 102 ms (76.85%) | Memory: 8.9 MB (53.70%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -1,36 +1,37 @@
 class Solution {
+    void solve(int idx, int cnt, vector<vector<int>>& requests, vector<int>& degree, int& ans){
+        if(idx == requests.size()){
+            // Check if this is a possible config
+            // bool flg = true;
+            for(auto& i : degree){
+                // cout<<i<<" ";
+                if(i != 0) {
+                    // flg = false;
+                    return;
+                }
+            }
+            // cout<<" -> "<<cnt<<endl;
+            // if(flg) 
+            ans = max(ans,cnt);
+            return;
+        }
+
+        // If i consider request[idx]
+        degree[requests[idx][0]]--;
+        degree[requests[idx][1]]++;
+        solve(idx+1, cnt+1, requests, degree, ans);
+        
+        degree[requests[idx][0]]++;
+        degree[requests[idx][1]]--;
+
+        // If i do not consider request[idx]
+        solve(idx+1, cnt, requests, degree, ans);
+    }
 public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        int answer = 0; 
-        
-        for (int mask = 0; mask < (1 << requests.size()); mask++) {
-            vector<int> indegree(n, 0);
-            int pos = requests.size() - 1;
-            int bitCount = __builtin_popcount(mask);
-            
-            if (bitCount <= answer) {
-                continue;
-            }
-            for (int curr = mask; curr > 0; curr >>= 1, pos--) {
-                if (curr & 1) {
-                    indegree[requests[pos][0]]--;
-                    indegree[requests[pos][1]]++;
-                }
-            }
-            
-            int flag = 1;
-            for (int i = 0; i < n; i++) {
-                if (indegree[i]) {
-                    flag = 0;
-                    break;
-                }
-            }
-            
-            if (flag)  {
-                answer = bitCount;
-            }
-        }
-        
-        return answer;
+        int ans = 0;
+        vector<int> degree(n, 0);
+        solve(0, 0, requests, degree, ans);
+        return ans;
     }
 };
