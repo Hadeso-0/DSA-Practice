7a1448172db24f1a6b51ad617248ba92eabfa780
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-11 12:26:56 +0530

    Time: 249 ms (32.95%) | Memory: 79 MB (41.48%) - LeetSync

--- contains-duplicate-iii/contains-duplicate-iii.cpp
+++ contains-duplicate-iii/contains-duplicate-iii.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
         int n = nums.size();
+        set<int> vals;
+        for(int i=0; i<n;++i){
+            if(vals.size() > indexDiff) vals.erase(vals.find(nums[i - indexDiff-1]));
+            // 
+            auto left = vals.lower_bound(nums[i] - valueDiff);
+            if((left != vals.end()) && (abs(*(left) - nums[i]) <= valueDiff)) return true;
+                
+            auto right = vals.upper_bound(nums[i] + valueDiff);
+            if((right != vals.begin()) && (abs(*prev(right) - nums[i]) <= valueDiff)) return true;
 
-        unordered_map<int,int> bucket;
-        for(int i=0; i<n; ++i){
-            int bucketNum = nums[i]/(valueDiff+1);
-            if(nums[i] < 0) --bucketNum;
-
-            if(bucket.find(bucketNum) != bucket.end()) return true;
-            else{
-                bucket[bucketNum] = nums[i];
-                if((bucket.find(bucketNum-1) != bucket.end()) && ((nums[i] - bucket[bucketNum-1]) <= valueDiff)){
-                    return true;
-                }
-                if((bucket.find(bucketNum+1) != bucket.end()) && ((bucket[bucketNum+1] - nums[i]) <= valueDiff)){
-                    return true;
-                }
-            }
-            if(bucket.size() > indexDiff){
-                int to_be_rem = nums[i-indexDiff] / (valueDiff+1);
-                if(nums[i-indexDiff] < 0) --to_be_rem;
-
-                bucket.erase(to_be_rem);
-            }
+            vals.insert(nums[i]);
         }
         return false;
     }
