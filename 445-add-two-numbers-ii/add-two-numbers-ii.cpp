aa971ef19c3ff9043eb545e1e08d78ac272ef021
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-17 13:36:25 +0530

    Time: 59 ms (5.65%) | Memory: 73.8 MB (14.05%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -43,10 +43,44 @@ class Solution {
     }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* l1_new = reverseList(l1);
-        ListNode* l2_new = reverseList(l2);
+        // ListNode* l1_new = reverseList(l1);
+        // ListNode* l2_new = reverseList(l2);
 
-        ListNode* ans = addLists(l1_new, l2_new);
-        return reverseList(ans);
+        // ListNode* ans = addLists(l1_new, l2_new);
+        // return reverseList(ans);
+
+        stack<int> s1, s2;
+
+        while (l1 != nullptr) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+
+        while (l2 != nullptr) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
+        int totalSum = 0, carry = 0;
+        ListNode* ans = new ListNode();
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) {
+                totalSum += s1.top();
+                s1.pop();
+            }
+            if (!s2.empty()) {
+                totalSum += s2.top();
+                s2.pop();
+            }
+
+            ans->val = totalSum % 10;
+            carry = totalSum / 10;
+            ListNode* newNode = new ListNode(carry);
+            newNode->next = ans;
+            ans = newNode;
+            totalSum = carry;
+        }
+
+        return carry == 0 ? ans->next : ans;
     }
 };
