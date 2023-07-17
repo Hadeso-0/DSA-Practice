735ff7f1f9951d06ca8f6825b5c4d7833b93ec56
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-17 13:34:58 +0530

    Time: 54 ms (11.01%) | Memory: 71.3 MB (35.96%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -20,38 +20,33 @@ class Solution {
 
         return prev;
     }
-    ListNode* addLists(ListNode* l1, ListNode* l2, int carry){
-        if((l1 == NULL) && (l2 == NULL)){
-            if(carry == 0) return NULL;
-
-            ListNode* temp = new ListNode(carry%10);
-            temp->next = addLists(l1, l2, carry/10);
-            return temp;
+    ListNode* addLists(ListNode* l1, ListNode* l2){
+        ListNode* ref = new ListNode(-1);
+        ListNode* tail = ref;
+        int carry = 0;
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum = 0;
+            if(l1 != NULL){
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum/10;
+            tail->next = new ListNode(sum%10);
+            tail = tail->next;
         }
-        else if(l1 == NULL){
-            l2->val = l2->val + carry;
-            l2->next = addLists(l1, l2->next, (l2->val)/10);
-            l2->val = (l2->val)%10;
-            return l2;
-        }
-        else if(l2 == NULL){
-            l1->val = l1->val + carry;
-            l1->next = addLists(l1->next, l2, (l1->val)/10);
-            l1->val = (l1->val)%10;
-            return l1;
-        }
-        
-        l1->val = l1->val + l2->val + carry;
-        l1->next = addLists(l1->next, l2->next, (l1->val)/10);
-        l1->val = (l1->val)%10;
-        return l1;
+        return ref->next;
     }
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* l1_new = reverseList(l1);
         ListNode* l2_new = reverseList(l2);
 
-        ListNode* ans = addLists(l1_new, l2_new, 0);
+        ListNode* ans = addLists(l1_new, l2_new);
         return reverseList(ans);
     }
 };
