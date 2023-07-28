0f659c83325e6d6c525e45327e520abeb24f4cee
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-28 17:06:19 +0530

    Time: 142 ms (99.64%) | Memory: 55.8 MB (71.43%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,26 +1,36 @@
 class Solution {
+    bool isPossible(int n, int m, vector<int>& bat, long long target){
+        long long tot_energy = 0LL;
+
+        for(int i=0; i<m; ++i){
+            tot_energy = tot_energy + min(bat[i]*1LL, target);
+        }
+
+        return tot_energy >= n*target;
+    }
 public:
     long long maxRunTime(int n, vector<int>& batteries) {
-        long long extra = 0LL;
+        long long sum = 0LL;
         int m = batteries.size();
-        vector<int> life;
-        sort(batteries.begin(), batteries.end());
         for(int i=0; i<m; ++i){
-            if(i < m-n){
-                extra += batteries[i];
+            sum += batteries[i];
+        }
+
+        long long l = 0LL;
+        long long r = sum/(long)n;
+        long long ans = -1;
+
+        while(l <= r){
+            long long mid = l + (r-l)/2LL;
+
+            if(isPossible(n, m, batteries, mid)){
+                ans = mid;
+                l = mid+1;
             }
             else{
-                life.push_back(batteries[i]);
+                r = mid-1;
             }
         }
-
-        for(int i=0; i<n-1; ++i){
-            if(extra < (long)(i+1)*(life[i+1]-life[i])){
-                return life[i] + extra/(long)(i+1);
-            }
-            extra -= (long)(i+1)*(life[i+1]-life[i]);
-        }
-
-        return life[n-1] + extra/(long)n;
+        return ans;
     }
 };
