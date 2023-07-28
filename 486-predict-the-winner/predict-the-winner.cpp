368cdcd5a46eb62ff0d7dc9660663cd22c5c9f1f
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-07-28 15:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (43.52%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -13,12 +13,23 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
         vector<int> prefix(n+1,0);
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         for(int i=0; i<n; ++i){
             prefix[i+1] = prefix[i] + nums[i];
         }
         long sum = prefix[n];
-        int p1 = solve(prefix, 1, n, dp);
+
+        for(int i=n; i>=1; --i){
+            dp[i][i] = prefix[i]-prefix[i-1]; 
+            for(int j=i+1; j<=n; ++j){
+
+                int tot = prefix[j] - prefix[i-1];
+                dp[i][j] = tot - min(dp[i+1][j], dp[i][j-1]);
+
+            }
+        }
+
+        int p1 = dp[1][n];
         return 2*p1 >= sum;
     }
 };
