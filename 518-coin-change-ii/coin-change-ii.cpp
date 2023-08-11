d9479adf66ebdebb4feafd71d8edc14449b6f489
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-08-11 13:51:26 +0530

    Time: 36 ms (45.64%) | Memory: 22 MB (9.06%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int hades(int amt, vector<int>& coins, int idx, vector<vector<int>>& dp){
+        if(dp[amt][idx] != -1) return dp[amt][idx];
+
+        if(amt == 0) return 1;
+        if(idx == coins.size()){
+            return 0;
+        }
+
+        int ans = 0;
+        if(coins[idx] <= amt){
+            ans += hades(amt-coins[idx], coins, idx, dp);
+        }
+        ans += hades(amt, coins, idx+1, dp);
+        
+        return dp[amt][idx] = ans;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        sort(coins.begin(), coins.end());
+        vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1, -1));
+        return hades(amount, coins, 0, dp);
+    }
+};
