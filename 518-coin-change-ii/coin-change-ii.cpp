55243f81477d69d95865336c65481fdcae12888f
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-08-11 13:56:25 +0530

    Time: 51 ms (31.05%) | Memory: 21.5 MB (12.50%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -18,7 +18,24 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         sort(coins.begin(), coins.end());
-        vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1, -1));
-        return hades(amount, coins, 0, dp);
+        // vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1, -1));
+        // return hades(amount, coins, 0, dp);
+
+        int n = coins.size();
+        vector<vector<int>> dp(amount+1, vector<int>(n+1, 0));
+        for(int i=0; i<=n; ++i){
+            dp[0][i] = 1;
+        }
+
+        for(int amt = 1; amt <= amount; ++amt){
+            for(int idx = 1; idx<=n; ++idx){
+                dp[amt][idx] = dp[amt][idx-1];
+                if(coins[idx-1] <= amt){
+                    dp[amt][idx] += dp[amt-coins[idx-1]][idx];
+                }
+            }
+        }
+
+        return dp[amount][n];
     }
 };
