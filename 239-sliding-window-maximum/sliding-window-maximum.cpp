cdeeef135a96e1d98730e62aad6d587048013c38
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-08-16 15:40:10 +0530

    Time: 213 ms (83.39%) | Memory: 134.7 MB (66.10%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,48 +1,34 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // vector<int> ans;
-        
-        // for(int i=k; i <= nums.size(); ++i){
-        //     int maxi = nums[i-k];
-        //     for(int j=i-k+1; j < i; ++j){
-        //         maxi = max(nums[j], maxi); 
-        //     } 
-        //     ans.push_back(maxi);
-        // }
-        // return ans;
-        // deque<int> dq;
-        // for(int i=0 ; i<k; ++i){
-        //     if(dq.empty()) dq.push_back(i);
-        //     else if(nums[dq.back()] >= nums[i]) dq.push_back(i);
-        //     else{
-        //         while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
-        //         dq.push_back(i);
-        //     }
-        // }
-        vector<int> ans;
-        // ans.push_back(nums[dq.front()]);
-        
-        // for(int i=k; i<nums.size(); ++i){
-        //     while(!dq.empty() && dq.front() <= (i-k)) dq.pop_front();
-            
-        //     if(dq.empty()) dq.push_back(i);
-        //     else if(nums[dq.back()] >= nums[i]) dq.push_back(i);
-        //     else{
-        //         while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
-        //         dq.push_back(i);
-        //     }
-        //     ans.push_back(nums[dq.front()]);
-        // }
-        multiset<int> st;
+        int n = nums.size();
+
+        deque<int> q;
         for(int i=0; i<k; ++i){
-            st.insert(nums[i]);
+            if(q.empty()) q.push_back(i);
+            else if(nums[i] <= nums[q.back()]) q.push_back(i);
+            else{
+                while(!q.empty() && (nums[i] > nums[q.back()])) q.pop_back();
+                q.push_back(i);
+            }
         }
-        ans.push_back(*(--st.end()));
-        for(int i=k; i<nums.size(); ++i){
-            st.erase(st.find(nums[i-k]));
-            st.insert(nums[i]);
-            ans.push_back(*(--st.end()));
+
+        vector<int> ans;
+        ans.push_back(nums[q.front()]);
+        for(int i=k; i<n; ++i){
+            
+            while(!q.empty() && (q.front() <= i-k)){
+                q.pop_front();
+            }
+            
+            if(q.empty()) q.push_back(i);
+            else if(nums[i] <= nums[q.back()]) q.push_back(i);
+            else{
+                while(!q.empty() && (nums[i] > nums[q.back()])) q.pop_back();
+                q.push_back(i);
+            }
+
+            ans.push_back(nums[q.front()]);
         }
 
         return ans;
