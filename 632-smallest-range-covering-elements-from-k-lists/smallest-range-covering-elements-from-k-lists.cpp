7700a6456a7d3d2b30e909e7dbc3e9114ffe2ed6
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-08-16 18:02:15 +0530

    Time: 117 ms (13.82%) | Memory: 18.4 MB (16.98%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        int n = nums.size(); 
+        priority_queue<vector<int>> pq;
+        int n = nums.size();
         
-        vector<int> ptrs(n, 0);
-        int rngS = INT_MAX;
-        int rngE = INT_MIN;
-        int rngD = INT_MAX;
-        
-        while(1){
-            int minV = INT_MAX;
-            int maxV = INT_MIN;
-            bool isOver = false;
-            
-            for(int i=0; i<n; ++i){
-                // cout<<i<<" "<<ptrs[i]<<"  ";
-                if(ptrs[i] >= nums[i].size()){
-                    isOver = true;
-                    break;
-                } 
-                minV = min(minV, nums[i][ptrs[i]]);
-                maxV = max(maxV, nums[i][ptrs[i]]);
-            }
-            cout<<endl;
-            if(isOver) break;
-            
-            if(rngD > (maxV-minV)){
+        int minV = INT_MAX; int maxV = INT_MIN;
+        for(int i=0; i<n; ++i){
+            pq.push({-nums[i][0], i, 0});
+            minV = min(minV, nums[i][0]);
+            maxV = max(maxV, nums[i][0]);
+        }
+
+        int rngS = minV; 
+        int rngE = maxV; 
+
+        while(!pq.empty()){
+            minV = -pq.top()[0];
+            int r = pq.top()[1];
+            int c = pq.top()[2];
+            pq.pop();
+
+            if((maxV - minV) < (rngE - rngS)){
                 rngS = minV;
                 rngE = maxV;
-                rngD = maxV - minV;
-            } 
-            
-            for(int i=0; i<n; ++i){
-                if(nums[i][ptrs[i]] == minV){
-                    ++ptrs[i];
-                }
+            }
+            if(c+1 < nums[r].size()){
+                pq.push({-nums[r][c+1], r, c+1});
+                maxV = max(maxV, nums[r][c+1]);
+            }
+            else{
+                break;
             }
         }
         return {rngS, rngE};
