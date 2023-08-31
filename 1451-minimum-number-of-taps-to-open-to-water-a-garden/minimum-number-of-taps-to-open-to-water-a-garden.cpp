439f676b67c8b0fa8acc56556805431e41f5a219
Manav Agarwal <52004311+Hadeso-0@users.noreply.github.com>
2023-08-31 14:54:04 +0530

    Time: 11 ms (90.50%) | Memory: 14.6 MB (55.15%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        int ans = 1e9;
-        vector<int> dp(n+1, ans);
+        // int ans = 1e9;
+        // vector<int> dp(n+1, ans);
 
-        dp[0] = 0;
-        for(int i=0; i<=n; ++i){
+        // dp[0] = 0;
+        // for(int i=0; i<=n; ++i){
+        //     int st = max(0, i-ranges[i]);
+        //     int et = min(n, i+ranges[i]);
+        //     for(int j = st; j<=et; ++j){
+        //         dp[et] = min(dp[et], dp[j]+1); 
+        //     }
+        // }
+
+        // if(dp[n] == ans) return -1;
+        // return dp[n];
+        vector<int> maxRch(n+1, 0);
+
+        for(int i=0; i<ranges.size(); ++i){
             int st = max(0, i-ranges[i]);
-            int et = min(n, i+ranges[i]);
-            for(int j = st; j<=et; ++j){
-                dp[et] = min(dp[et], dp[j]+1); 
-            }
+            int en = min(n, i+ranges[i]);
+
+            maxRch[st] = max(maxRch[st], en);
         }
 
-        if(dp[n] == ans) return -1;
-        return dp[n];
+        int ans = 0;
+        int curEnd = 0; int nextEnd = 0;
+
+        for(int i=0; i<=n; ++i){
+            if(i > nextEnd) return -1;
+            if(i > curEnd){ ++ans; curEnd = nextEnd; }
+            nextEnd = max(nextEnd, maxRch[i]);
+        }
+
+        return ans;
     }
 };
